64461aacc9c2c3c3c1b8bf8a77ff448b87b2dee8
Leif Högberg <leihog@gmail.com>
2013-11-22 18:33:57 +0100

    added chuck plugin

---
+++ plugins/chuck.php
@@ -0,0 +1,17 @@
+<?php
+namespace Bot\Plugin;
+use Bot\Bot as Bot;
+
+class Chuck extends Plugin
+{
+	public function cmdChuck($event)
+	{
+		$result = $this->get("http://api.icndb.com/jokes/random");
+		if ($result) {
+			$result = json_decode($result, true);
+			if (isset($result['joke'])) {
+				$event->respond($result['joke']);
+			}
+		}
+	}
+}
