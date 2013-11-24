d3cd2835ba691ecdb40895e0a72420ea07a5c253
Leif Högberg <leihog@gmail.com>
2013-11-24 18:51:38 +0100

    chuck plugin fix

--- plugins/chuck.php
+++ plugins/chuck.php
@@ -9,8 +9,8 @@ class Chuck extends Plugin
 		$result = $this->getUrl("http://api.icndb.com/jokes/random");
 		if ($result) {
 			$result = json_decode($result, true);
-			if (isset($result['joke'])) {
-				$event->respond($result['joke']);
+			if (isset($result['value']['joke'])) {
+				$event->respond(html_entity_decode($result['value']['joke']));
 			}
 		}
 	}
